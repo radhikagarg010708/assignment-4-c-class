fd1a2944dafb96cc5ef409df3d1bb6c99bf8a639
radhikagarg010708 <radhikagarg010708@gmail.com>
2026-09-04 02:15:15 +0530

    QUESTION 4

---
+++ assign4ques4.c
@@ -0,0 +1,45 @@
+// keyword identification
+#include <stdio.h> 
+int main()
+{
+    int marks;
+    float percentage;
+    char grade;
+    const int totalMarks = 500;
+
+    printf("Enter marks obtaned out of 500: ");
+    scanf("%d", &marks);
+    percentage = (float) marks / totalMarks * 100;
+
+    if (percentage >= 90)
+    {
+        grade = 'A';
+        printf("Excellent\n");
+
+    }
+
+else if (percentage >=75)
+{
+    grade = 'B';
+    printf("Very Good\n");
+}
+else if (percentage >= 60)
+{
+    grade = 'C';
+    printf("Good\n");
+}
+else if (percentage >= 40)
+ { 
+    grade = 'D';
+    printf("Pass\n");
+ }
+ else 
+ {
+    grade = 'F';
+    printf("Fail\n");
+ }
+      printf("Percentage = %.2f\n", percentage);
+      printf("Grade = %c\n", grade);
+
+      return 0;
+}
